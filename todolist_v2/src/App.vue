58533aaf7d21ec68ed74b17bc2b40d910b63848e
tashikomaaa <tashikomaa@gmail.com>
2017-10-05 17:54:23 +0200

    build V2

---
+++ todolist_v2/src/App.vue
@@ -0,0 +1,131 @@
+<template>
+  <div>
+    <header id="home">
+      <img src="./assets/logo.jpg" alt="">
+      <div id="container">
+      </div>
+    </header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+html,
+body,
+header,
+main {
+  height: 50vh;
+}
+
+ul {
+  list-style-type: none;
+}
+
+header {
+  background: -webkit-linear-gradient(top, #fff, grey);
+  background: linear-gradient(top left, #fff, grey);
+}
+
+.scroll {
+  position: absolute;
+  left: 50%;
+  bottom: 0;
+  height: 0;
+  width: 0;
+  border-left: 90px solid transparent;
+  border-right: 90px solid transparent;
+  border-bottom: 80px solid white;
+  -webkit-transform: translateX(-50%);
+  transform: translateX(-50%);
+}
+
+.scroll:before {
+  content: '';
+  position: absolute;
+  top: 45px;
+  left: 50%;
+  height: 20px;
+  width: 30px;
+  margin-top: -5px;
+  margin-left: -15px;
+  background: -webkit-linear-gradient(top left, #e84c00, #c10040);
+  background: linear-gradient(top left, #e84c00, #c10040);
+  -webkit-clip-path: polygon(15% 0, 50% 60%, 85% 0, 100% 20%, 50% 100%, 0 20%);
+  clip-path: polygon(15% 0, 50% 60%, 85% 0, 100% 20%, 50% 100%, 0 20%);
+  -webkit-animation: bounce 2s ease infinite;
+  animation: bounce 2s ease infinite;
+}
+
+header img {
+  margin-top: 10%;
+}
+
+@-webkit-keyframes bounce {
+  50% {
+    -webkit-transform: scale(0.75) translateY(-50%);
+    transform: scale(0.75) translateY(-50%);
+  }
+  100% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+  }
+}
+
+@keyframes bounce {
+  50% {
+    -webkit-transform: scale(0.75) translateY(-50%);
+    transform: scale(0.75) translateY(-50%);
+  }
+  100% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+  }
+}
+
+a {
+  text-decoration: none;
+}
+
+/* This class is added on scroll */
+.fixed {
+  position: fixed;
+  top: 0;
+  height: 70px;
+  z-index: 1;
+}
+
+body {
+  color: #fff;
+  font-family: 'open-sans-bold', AvenirNext-Medium, sans-serif;
+  font-size: 18px;
+  text-align: center;
+}
+
+/* Navigation Settings */
+#screen1 {
+  background: #43b29d;
+  height: 100%;
+}
+
+#screen1 p {
+  padding-top: 100%;
+}
+
+#container {
+  position: absolute;
+  top: 65%;
+  left: 48%;
+}
+
+#input {
+  position: relative;
+  margin-top: -20px;
+  margin-left: -120px;
+}
+</style>
---
+++ todolist_v2/src/components/Hello.vue
@@ -0,0 +1,65 @@
+<template>
+  <section id="screen1">
+    <div class="card-container">
+      <h2>TODO</h2>
+      <div class="calendar-card card-1" style="color: black!important;">
+        <div class="container" id="todo">
+          <!-- Panel for holding our input -->
+          <section class="panel">
+            <form v-on:submit.prevent="addTask">
+              <input type="text" placeholder="Task title" class="text-input" v-model="newTaskTitle">
+              <input type="text" placeholder="What do you need to do?" class="text-input" v-model="newTaskContent">
+              <button type="submit">Create</button>
+            </form>
+            <button v-on:click="clearList">Clear List</button>
+          </section>
+          <div class="card-container ">
+            <div class="card card-1" v-for=" (task, index) in taskList ">
+              <div class="card-topnew">
+                {{ task.title }}
+              </div>
+              {{ task.text }}
+              <hr>
+              <button class="btn btn-border color-1 material-design" data-color="#2f5398" v-on:click="open(index)">option</button>
+              <div class="selectStatus hidden" :id="index">
+                <button class="btn btn-border color-2 material-design" data-color="#d2181c" onclick="done(this)">done</button>
+                <hr>
+                <button class="btn btn-border color-1 material-design" data-color="#d2181c" onclick="progress(this)">progress</button>
+                <hr>
+                <button class="btn btn-border color-4 material-design" data-color="#d2181c" v-on:click="deleteTask(task, index)">delete</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'hello'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
